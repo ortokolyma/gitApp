93c15705266816ed4ebb0a33b4df5c9ed2d92b03
Andrey Isaev <andron.isaev@gmail.com>
2020-04-13 19:21:13 +0500

    ver 1 commit 1

--- LocalGinApp2/ViewController.swift
+++ LocalGinApp2/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("ver 1")
+        print("commit 1")
+        
+        
     }
 
 
